84507db92ec5c7cd4308e8b45008def847b58ed7
Aaron Tam <60371257+Homestuck126@users.noreply.github.com>
2021-11-07 00:42:15 +0000

    Added emote counter and Map

--- main.py
+++ main.py
@@ -1,11 +1,99 @@
 import discord
 import os
+import asyncio
 from discord.ext import commands
-import map
+from map import MapNode
+from map import Map
+from discord.utils import get
 
+start = False
 secret = os.environ['Token']
+def generatemap():
+  Home = MapNode("This is Home")
+  Uptown = MapNode("This is UpTown")
+  Home.insert(Uptown, 1)
+  Downtown = MapNode("This is DownTown")
+  Home.insert(Downtown, 4)
+  Current = Map(Home)
+  return Current
+
 
 client = commands.Bot(command_prefix = "$")
+@client.event
+async def on_raw_reaction_add(payload):
+  channel = client.get_channel(906579155238395916)
+  (payload.message_id)
+  message = await channel.fetch_message(payload.message_id)
+  if payload.emoji.name == "◀️":
+    reaction = get(message.reactions, emoji="◀️")
+    global leftcount
+    leftcount = reaction.count
+  if payload.emoji.name == "▶️":
+    reaction = get(message.reactions, emoji="▶️")
+    global rightcount
+    rightcount = reaction.count
+  if payload.emoji.name == "🔽":
+    reaction = get(message.reactions, emoji="🔽")
+    global downcount
+    downcount = reaction.count
+  if payload.emoji.name == "🔼":
+    reaction = get(message.reactions, emoji="🔼")
+    global upcount 
+    upcount = reaction.count
+
+@client.event
+async def adventure(message , Current):
+  await message.channel.send("Welcome to your grand new adventure, work together with your friends to explore the world")
+  while True:
+    output = await message.channel.send(Current.Curr.dataval)
+    await output.add_reaction("▶️")
+    await output.add_reaction("◀️")
+    await output.add_reaction("🔽")
+    await output.add_reaction("🔼")
+    await asyncio.sleep(10)
+    up = upcount
+    down = downcount
+    right = rightcount
+    left = leftcount
+    await message.channel.send(up)
+    await message.channel.send(down)
+    await message.channel.send(right)
+    await message.channel.send(left)
+    check = False
+    numbers = [right,left,up,down]
+    numbers.sort(reverse=True)
+    if (numbers[0] == numbers[1]):
+      check = False
+    elif(max(numbers) == up):
+      if Current.Curr.up is None:
+        check = False
+      else:
+        check =Current.goNext(1)
+        await message.channel.send('you went North')
+    elif(max(numbers) == right):
+      if Current.Curr.right is None:
+        check = False
+      else:
+        check =Current.goNext(2)
+        await message.channel.send('you went East')
+    elif(max(numbers) == left):
+      if Current.Curr.left is None:
+        check = False
+      else:
+        check =Current.goNext(3)
+        await message.channel.send('you went West')
+    elif(max(numbers) == down):
+      if Current.Curr.down is None:
+        check = False
+      else:
+        check =Current.goNext(4)
+        await message.channel.send('you went south')
+
+    if check is False:
+      await message.channel.send('cannot go that way')
+
+    
+
 
 
 @client.event
@@ -24,9 +112,12 @@ async def on_message(message):
   if msg.startswith('$hi'):
     await message.channel.send('Hello!')
 
-  if msg.startswith('$netscape map'):
-    await message.channel.send(map.output_map())
 
+  if msg.startswith('$adventure'):  
+    client.loop.create_task(adventure(message , generatemap()))
+    await message.channel.send('The adventure has begun.')
+  
+      
   
 client.run(secret)
 
--- map.py
+++ map.py
@@ -1,24 +1,41 @@
-from replit import db
-
 
 class MapNode:
-  dataval =""
-  Up = None
-  Down = None
-  Right = None
-  Left = None
-  
-
   def __init__(self, f):
     self.dataval = f
+    self.up = None
+    self.down = None
+    self.right = None
+    self.left = None
     
-  def print(self):
-    return self.dataval
-    
-Home = MapNode("This is Home")
-db["map"] = Home
-Home.print
+  def insert (self, MapNode , direction):
+    if direction == 1:
+      self.up = MapNode
+      MapNode.down = self
+    elif direction == 2:
+      self.right = MapNode
+      MapNode.left = self
+    elif direction == 3:
+      self.left = MapNode
+      MapNode.right = self
+    elif direction == 4:
+      self.down = MapNode
+      MapNode.up = self
 
-def output_map():
-  return db[map].print
-
+class Map:
+  def __init__(self, f):
+    self.Curr = f
+  def goNext(self, input):
+    if(input == 1):
+      self.Curr = self.Curr.up
+      return True
+    elif(input == 2):
+      self.Curr = self.Curr.right
+      return True
+    elif(input == 3):
+      self.Curr = self.Curr.left
+      return True
+    elif(input == 4):
+      self.Curr = self.Curr.down
+      return True
+    else:
+      return False
